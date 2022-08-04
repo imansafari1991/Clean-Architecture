30c71e866ddfd55225d8c67fa84a03e6d7abb2ec
iman.safari <iman.safari@gardenoflove.co>
2022-08-04 15:46:42 +0430

    add application layer and persistence

---
+++ HR.LeaveManagement/src/Core/HR.LeaveManagement.Application/Persistence/Contracts/ILeaveTypeRepository.cs
@@ -0,0 +1,9 @@
+﻿using HR.LeaveManagement.Domain;
+
+namespace HR.LeaveManagement.Application.Persistence.Contracts
+{
+    public interface ILeaveTypeRepository:IGenericRepository<LeaveType>
+    {
+        
+    }
+}
